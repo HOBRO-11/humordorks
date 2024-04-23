12371b159a8e6262337415a0fb25dc4986b5f5ee
Hobro <hobro3710@naver.com>
2024-04-23 14:30:19 +0900

    create common service

---
+++ src/main/java/com/mal/humordorks/service/CommentCommonService.java
@@ -0,0 +1,23 @@
+package com.mal.humordorks.service;
+
+import org.springframework.data.domain.Page;
+
+import com.mal.humordorks.model.Comment;
+import com.mal.humordorks.model.Member;
+import com.mal.humordorks.model.Posts;
+
+public interface CommentCommonService {
+
+    Comment createComment(Member member, Posts posts, String content);
+
+    Comment findComment(long id);
+
+    Page<Comment> findCommentsByPosts(Posts posts, int page, int size);
+
+    Page<Comment> findCommentsByMember(Member member, int page, int size);
+
+    void modifyComment(Member member, Comment comment, String content);
+
+    void deleteComment(Member member, Comment comment);
+
+}
---
+++ src/main/java/com/mal/humordorks/service/PostsCommonService.java
@@ -0,0 +1,30 @@
+package com.mal.humordorks.service;
+
+import org.springframework.data.domain.Page;
+
+import com.mal.humordorks.dto.PostsRegisterForm;
+import com.mal.humordorks.model.Member;
+import com.mal.humordorks.model.Posts;
+
+public interface PostsCommonService {
+
+    Posts createPosts(Member member, PostsRegisterForm postsRegisterForm);
+
+    Posts findPosts(long id);
+
+    Page<Posts> findAllPosts(int page, int size);
+
+    void modifiedPosts(Member member, Posts posts, String content);
+
+    // TODO elastic search 를 써서 찾는 인덱스를 만든다.
+
+    // TODO only just admin can hide posts
+    void hidePost(Posts posts);
+
+    void hidePost(Member member, Posts posts);
+
+    void publicPost(Member member, Posts posts);
+
+    void deletePosts(Member member, Posts posts);
+
+}
