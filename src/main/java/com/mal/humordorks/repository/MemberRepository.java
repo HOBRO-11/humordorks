a8a7ff301d6d84cd03baa16f6ace3f9d68ed913b
Hobro <hobro3710@naver.com>
2024-04-23 16:57:47 +0900

    update repository

--- src/main/java/com/mal/humordorks/repository/MemberRepository.java
+++ src/main/java/com/mal/humordorks/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.mal.humordorks.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
 
 	boolean existsByNickname(String nickname);
+
+    Optional<Member> findByEmail(String email);
     
 }
