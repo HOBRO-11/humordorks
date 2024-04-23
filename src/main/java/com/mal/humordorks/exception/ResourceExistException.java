de38fddb98aade9872c89ddd45daaa6963438d1b
Hobro <hobro3710@naver.com>
2024-04-23 19:50:06 +0900

    update exception

---
+++ src/main/java/com/mal/humordorks/exception/ResourceExistException.java
@@ -0,0 +1,8 @@
+package com.mal.humordorks.exception;
+
+public class ResourceExistException extends RuntimeException{
+
+    public ResourceExistException(String message){
+        super(message);
+    }
+}
