f2280e50de3c64d38b33274fb172c2e6e56fb8a2
Hobro <hobro3710@naver.com>
2024-04-23 19:50:30 +0900

    update repository

--- src/main/java/com/mal/humordorks/repository/PostsRepository.java
+++ src/main/java/com/mal/humordorks/repository/PostsRepository.java
@@ -1,9 +1,18 @@
 package com.mal.humordorks.repository;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mal.humordorks.model.Posts;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     
+    @Query("SELECT p FROM Posts p WHERE p.created_date >= :start_date AND p.created_date <= :end_date")
+    Page<Posts> findPopularPosts(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
+
 }
