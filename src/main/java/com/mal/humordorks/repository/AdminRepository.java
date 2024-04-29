04af7f2849fb6e9f7e6e90bca4793ca3f36bcb8d
Hobro <hobro3710@naver.com>
2024-04-29 17:12:01 +0900

    update repository

--- src/main/java/com/mal/humordorks/repository/AdminRepository.java
+++ src/main/java/com/mal/humordorks/repository/AdminRepository.java
@@ -1,5 +1,7 @@
 package com.mal.humordorks.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +12,7 @@ import com.mal.humordorks.model.AdminRole;
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     Page<Admin> findAllByAdminRole(AdminRole role, Pageable pageable);
+
+    Optional<Admin> findByEmail(String email);
 
 }
